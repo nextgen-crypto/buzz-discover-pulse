1592e2c162e81f875ee4bc4d65096044cf1320d3
Nextgen Crypto <nextgen12222@gmail.com>
2026-09-21 09:16:04 +0300

    Switch to Nitro for Vercel

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,16 @@
-import { defineConfig } from "@lovable.dev/vite-tanstack-config";
+import { defineConfig } from 'vite'
+import { tanstackStart } from '@tanstack/react-start/plugin/vite'
+import { nitro } from 'nitro/vite'
+import viteReact from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
+import tsConfigPaths from 'vite-tsconfig-paths'
 
 export default defineConfig({
-  tanstackStart: {
-    server: { entry: "server" },
-    spa: {
-      enabled: true,
-      prerender: {
-        crawlLinks: true,
-        outputPath: "index.html",
-      },
-    },
-  },
-  tailwindcss: {
-    optimize: false,
-  },
-});
+  plugins: [
+    tsConfigPaths(),
+    tailwindcss(),
+    tanstackStart(),
+    nitro({ preset: 'vercel' }),
+    viteReact(),
+  ],
+})
